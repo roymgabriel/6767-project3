4237dfa5c5595f82db2555b349ab18a011cef728
roymgabriel <roy.makkar@gmail.com>
2022-12-10 13:18:27 -0500

    Check the TODO

--- Factors.py
+++ Factors.py
@@ -3,82 +3,87 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
+
 class Factors():
-   def __init__(self, window, start, finish, coins_file='data/coin_universe_150K_40.csv', prices_file='data/coin_all_prices.csv'):
-      # Reading in symbols for the period
-      self.symbols_df = pd.read_csv(coins_file)
-      self.symbols_df = self.__clean_dates(self.symbols_df)
-      self.symbols_df = self.symbols_df[(self.symbols_df.index >= start) & (self.symbols_df.index <= finish)]
+    def __init__(self, window, start, finish, coins_file='data/coin_universe_150K_40.csv',
+                 prices_file='data/coin_all_prices.csv'):
+        # Reading in symbols for the period
+        self.symbols_df = pd.read_csv(coins_file)
+        self.symbols_df = self.__clean_dates(self.symbols_df)
+        self.symbols_df = self.symbols_df[(self.symbols_df.index >= start) & (self.symbols_df.index <= finish)]
 
-      # Reading in prices from csv
-      self.prices_df = pd.read_csv(prices_file)
-      self.prices_df = self.__clean_dates(self.prices_df)
-      self.prices_df = self.__clean_data(self.prices_df)
+        # Reading in prices from csv
+        self.prices_df = pd.read_csv(prices_file)
+        self.prices_df = self.__clean_dates(self.prices_df)
+        self.prices_df = self.__clean_data(self.prices_df)
 
-      # Getting the returns from the prices dataframe
-      self.returns_df = self.__get_rets(self.prices_df)
+        # Getting the returns from the prices dataframe
+        self.returns_df = self.__get_rets(self.prices_df)
 
-      # Additional parameters
-      self.M = window
-      self.start = start
-      self.finish = finish
-      self.asset_std = None
-      self.eigenvalues = None
-      self.hourly_rets = None
+        # Additional parameters
+        self.M = window
+        self.start = start
+        self.finish = finish
+        self.asset_std = None
+        self.eigenvalues = None
+        self.hourly_rets = None
 
+        self.Q_j = self.get_Q()
+        self.F_jk = self.get_factor_return()
 
-      # TODO: change self.finish to something better
-      self.Q_j = self.get_Q()
-      self.F_jk = self.get_factor_return()
+    def __clean_dates(self, df):
+        df['startTime'] = pd.to_datetime(df['startTime'].apply(lambda x: x.split(':')[0]), format='%Y-%m-%dT%H')
+        df = df.set_index('startTime').drop('time', axis=1)
+        return df
 
-   def __clean_dates(self, df):
-      df['startTime'] = pd.to_datetime(df['startTime'].apply(lambda x : x.split(':')[0]), format='%Y-%m-%dT%H')
-      df = df.set_index('startTime').drop('time', axis=1)
-      return df
+    def __clean_data(self, df):
+        df.ffill(inplace=True)
+        return df
 
-   def __clean_data(self, df):
-      df.ffill(inplace=True)
-      return df
+    def __get_rets(self, prices_df):
+        """
+      Compute factor returns of the two risk factors at time t.
+      :return:
+      """
+        ret_df = prices_df.pct_change()
+        return ret_df
 
-   def __get_rets(self, prices_df):
-      ret_df = prices_df.pct_change()
-      return ret_df
+    def get_standardize_rets(self, df):
+        self.asset_std = df.std()  # could also do np.sqrt(scaler.var_)
+        scaler = StandardScaler()
+        return pd.DataFrame(scaler.fit_transform(df), index=df.index, columns=df.columns)
 
-   def get_standardize_rets(self, df):
-      self.asset_std = df.std()  # could also do np.sqrt(scaler.var_)
-      scaler = StandardScaler()
-      return pd.DataFrame(scaler.fit_transform(df), index=df.index, columns=df.columns)
+    def get_corr_mat(self, df):
+        st_rets = self.get_standardize_rets(df)
+        return st_rets.corr()
 
-   def get_corr_mat(self, df):
-      st_rets = self.get_standardize_rets(df)
-      return st_rets.corr()
+    def get_pca(self, df):
+        emp_corr = self.get_corr_mat(df)
+        pca_model = PCA(n_components=2)
+        pca_model.fit(emp_corr)
+        self.eigenvalues = pca_model.explained_variance_
+        eigenvectors = pd.DataFrame(pca_model.components_, columns=df.columns)
+        # Returns the eigen vectors
+        return eigenvectors
 
-   def get_pca(self, df):
-      emp_corr = self.get_corr_mat(df)
-      pca_model = PCA(n_components=2)
-      pca_model.fit(emp_corr)
-      self.eigenvalues = pca_model.explained_variance_
-      eigenvectors = pd.DataFrame(pca_model.components_, columns=df.columns)
-      # Returns the eigen vectors
-      return eigenvectors
+    def get_Q(self):
+        used_symbols = list(self.symbols_df.loc[self.start])
+        time_idx = self.returns_df.index.get_loc(self.start)
+        self.hourly_rets = self.returns_df[used_symbols].iloc[time_idx - self.M: time_idx]
+        st_rets = self.get_standardize_rets(self.hourly_rets)
+        pca_eigenvectors = self.get_pca(st_rets)
+        Q = pca_eigenvectors / self.asset_std
+        return Q
 
-   def get_Q(self):
-      used_symbols = list(self.symbols_df.loc[self.start])
-      time_idx = self.returns_df.index.get_loc(self.start)
-      self.hourly_rets = self.returns_df[used_symbols].iloc[time_idx - self.M : time_idx]
-      st_rets = self.get_standardize_rets(self.hourly_rets)
-      pca_eigenvectors = self.get_pca(st_rets)
-      Q = pca_eigenvectors / self.asset_std
-      return Q
-
-   def get_factor_return(self):
-      # Q is 2x40 and hourly_rets 239x40
-      return self.Q_j@self.hourly_rets.T
+    def get_factor_return(self):
+        # Q is 2x40 and hourly_rets 239x40
+        return self.Q_j @ self.hourly_rets.T
 
 
 def main():
-   test = Factors(240, '2021-03-08 05:00:00', '2022-09-25 23:00:00')
-   print(test.symbols_df.head())
+    test = Factors(240, '2021-03-08 05:00:00', '2022-09-25 23:00:00')
+    print(test.symbols_df.head())
+
 
 if __name__ == "__main__":
-   main()
+    main()
--- StatArbStrategy.py
+++ StatArbStrategy.py
@@ -29,20 +29,6 @@ class StatArbStrategy:
         self.X_l, self.xl_residuals = self.get_X_l()
         self.params_df = self.get_params()
 
-    def get_factor_returns(self):
-        """
-        Compute factor returns of the two risk factors at time t.
-        :return:
-        """
-
-        pass
-
-    def get_PCA(self):
-        """
-        Apply Principal Component Anlysis (PCA) and get top two pc vectors along with their eigenvalues.
-        :return:
-        """
-        return None
 
     def estimate_resid_returns(self):
         """
@@ -66,14 +52,12 @@ class StatArbStrategy:
         Function to get X_l in order to obtain a and b parameters
         :return:
         """
-        ''' FINISHED '''
-        # TODO: We need to get the residuals of the future time (t + dt) as well and do regression on those
         # NOTE: this function should return parameters a and b for each asset
-        X_l = self.residuals.expanding().sum()
+        X_l = self.residuals.cumsum()
         X_l_shift = X_l.shift(-1)
         regressor = LinearRegression()
-        X = X_l[:-1] # M-1 rows
-        y = X_l_shift[:-1] # M-1 rows
+        X = X_l[:-1]  # M-1 rows
+        y = X_l_shift[:-1]  # M-1 rows
         coeffs = {}
         xl_residuals = {}
         for col in y.columns:
@@ -93,10 +77,6 @@ class StatArbStrategy:
         A function to get greek parameters
         :return:
         """
-        #TODO: we can either loop here or in init but basically we loop through each asset and get their
-        # a and b and resid from self.xxx, then you do  the calculations below
-        # we can store the results in a dictionary for each asset for later functions
-
         # I changed the function to loop through each currency and get the parameters for each one
         params = {}
         for col in self.X_l.columns:
@@ -105,7 +85,7 @@ class StatArbStrategy:
             b = self.X_l.loc['b', col]
             resid = self.xl_residuals[col]
 
-            kappa = -np.log(b) * 8760 # For the number of hours in a year
+            kappa = -np.log(b) * 8760  # For the number of hours in a year
             m = a / (1-b)
             sigma = np.sqrt(np.var(resid) * 2 * kappa / (1 - b**2))
             sigma_eq = np.sqrt(np.var(resid) / (1 - b**2))
@@ -117,27 +97,16 @@ class StatArbStrategy:
         Calculate s-score at time t.
         :return:
         """
-        # TODO: X(t) is 0 for the current time period so the real calculation is s = -m / sigma_eq with the m centered
         cpy = params_df.copy(deep=True)
         m_mean = cpy['m'].mean()
         s_score = (m_mean - cpy['m']) / cpy['sigma_eq']
         return s_score
-
-
-    def deploy_regressor(self):
-        """
-        Apply linear regression and get parameters.
-        :return:
-        """
-        pass
 
     def generate_trading_signals(self, s_score):
         """
         Generate trading signals at time t.
         :return:
         """
-        # TODO: We need to somehow translate those strings into actually going long or short these stocks, where do the
-        # weights come in? maybe Q changes idk
         s_bo = 1.25
         s_so = 1.25
         s_bc = 0.75
@@ -156,8 +125,6 @@ class StatArbStrategy:
 
         return pd.Series(np.select(conditions, values), name='Signals', index=s_score.index)
 
-
-
     def evaluate_strat(self):
         """
         Evaluate strategy performance over a testing period.
@@ -172,19 +139,22 @@ class StatArbStrategy:
         """
         pass
 
-    def get_SR(self):
+    def get_SR(self, stats_df: pd.DataFrame, rf: float):
         """
         Compute Sharpe Ratio.
         :return:
         """
-        pass
+        return (stats_df.loc['mean'] - rf)/stats_df.loc['std']
 
-    def get_MDD(self):
+    def get_MDD(self, df: pd.DataFrame, col: str, window=12, min_periods=1):
         """
         Compute maximum draw-down (MDD).
         :return:
         """
-        pass
+        # Change min_periods if you want to let the first X days data have an expanding window
+        max_rolling_window = df[col].rolling(window, min_periods=1).max()
+        DD = df[col]/max_rolling_window - 1.0
+        return DD.rolling(window, min_periods=1).min()
 
     def plot_eigen_portfolio(self):
         """
